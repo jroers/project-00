cf1dbdb5961ee459de7643b94b1da3120ce07e25
Joe Roers <Joe@Joe-Roers-MacBook-Pro.local>
2015-11-29 22:44:21 -0800

    minor changes. Removed unnecessary elements, sanity check

--- game-2/main.js
+++ game-2/main.js
@@ -1,14 +1,13 @@
-console.log("Sanity Check: JS is working!");
+//console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
-	var $player1 = $(".player1");
-	var player1upDown = -15;
-	var player1leftRight = 300;
-
-	var $player2 = $(".player2");
-	var player2upDown = -48;
-	var player2leftRight = 300;
+	//if each quarter contains a value of 1, then the number of laps will increase.
+	function Racer() {
+		this.quarter1 = 0;
+		this.quarter2 = 0;
+		this.quarter3 = 0;
+		this.lap = 0;
+	}
 
 	//player 1 move right
 	function pl1moveRight() {
@@ -65,15 +64,17 @@ $(document).ready(function(){
 		$player2.css("top", player2upDown + "px");
 	}
 
-	//Since I wanted to use arrow keys, I was able to find out the values of the arrow keys here: http://jsfiddle.net/JamesD/8q7Mu/
-	$(window).keydown(function keypressHandler(event) {
-		//Button inputs for Player 1 - WASD mapping)
-		if (event.which === 68 || event.which === 83 || event.which === 65 || event.which === 87) {
-			//This if statement didn't work. I wanted to prevent the player from entering the middle ground, but it gets locked once you get too close. Need to troubleshoot.
-			// Maybe change the <div class="racetrack"> to a <canvas>?
-			// if (player1upDown + 20 > 45 && player1upDown -20 < 345 && player1leftRight + 20 > 80 && player1leftRight - 20 < 620) {
-			// 	console.log("movement not allowed");
-			// } else {
+	function startRace() {
+		//Since I wanted to use arrow keys, I was able to find out the values of the arrow keys here: http://jsfiddle.net/JamesD/8q7Mu/
+		$(window).keydown(function keypressHandler(event) {
+			//Button inputs for Player 1 - WASD mapping)
+			if (event.which === 68 || event.which === 83 || event.which === 65 || event.which === 87) {
+				//This if statement didn't work. I wanted to prevent the player from entering the middle ground, but it gets locked once you get too close. Need to troubleshoot.
+				// Maybe change the <div class="racetrack"> to a <canvas>?
+				// if (player1upDown + 20 > 45 && player1upDown -20 < 345 && player1leftRight + 20 > 80 && player1leftRight - 20 < 620) {
+				// 	console.log("movement not allowed");
+				// } else {
+
 				//Right - D
 				if (event.which === 68) {
 					if (player1leftRight + 20 <= 720 && player1upDown < 345) {
@@ -95,32 +96,119 @@ $(document).ready(function(){
 						pl1moveUp();
 					}
 				}
-			// }
-		}
-		
-		//Button inputs for Player 2 - Arrow key mapping
-		//Right arrow
-		if (event.which === 39) {
-			if (player2leftRight + 20 <= 720 && player2upDown < 252) {
-				pl2moveRight();
+
+				// }
 			}
-		//Down arrow
-		} else if (event.which === 40) {
-			if (player2upDown + 20 <= 332 && player2leftRight > 80) {
-				pl2moveDown();
+			
+			//Button inputs for Player 2 - Arrow key mapping
+			//Right arrow
+			if (event.which === 39) {
+				if (player2leftRight + 20 <= 720 && player2upDown < 252) {
+					pl2moveRight();
+				}
+			//Down arrow
+			} else if (event.which === 40) {
+				if (player2upDown + 20 <= 332 && player2leftRight > 80) {
+					pl2moveDown();
+				}
+			//Left arrow
+			} else if (event.which === 37) {
+				if (player2leftRight - 20 >= -20 && player2upDown > -48) {
+					pl2moveLeft();
+				}
+			//Up arrow
+			} else if (event.which === 38 ) {
+				if (player2upDown - 20 >= -128 && player2leftRight < 620) {
+					pl2moveUp();
+				}
 			}
-		//Left arrow
-		} else if (event.which === 37) {
-			if (player2leftRight - 20 >= -20 && player2upDown > -48) {
-				pl2moveLeft();
-			}
-		//Up arrow
-		} else if (event.which === 38 ) {
-			if (player2upDown - 20 >= -128 && player2leftRight < 620) {
-				pl2moveUp();
-			}
-		}
-		
+
+			//Player 1 win conditions
+			if (player1upDown === 205 && player1leftRight >= 620 && player1.quarter1 === 0) {
+				player1.quarter1 = 1;
+			} else if (player1leftRight === 340 && player1upDown >= 345 && player1.quarter2 === 0) {
+				player1.quarter2 = 1;
+			} else if (player1upDown === 205 && player1leftRight <= 80 && player1.quarter3 === 0) {
+				player1.quarter3 = 1;
+			} else if (
+				player1leftRight === 340 &&
+				player1upDown <= 45 && 
+				player1.quarter3 === 1 && 
+				player1.quarter2 === 1 && 
+				player1.quarter1 === 1
+				)
+			{
+				player1.lap = 1;
+				$(".winner").html("<h1>Player 1 wins!</h1>");
+				$(window).off("keydown");
+				$("button").show();
+			
+			//Player 2 win conditions
+			} else if (player2upDown === 92 && player2leftRight >= 620 && player2.quarter1 === 0) {
+				player2.quarter1 = 1;
+			} else if (player2leftRight === 340 && player2upDown >= 252 && player2.quarter2 === 0) {
+				player2.quarter2 = 1;
+			} else if (player2upDown === 92 && player2leftRight <= 80 && player2.quarter3 === 0) {
+				player2.quarter3 = 1;
+			} else if (
+				player2leftRight === 340 &&
+				player2upDown <= -48 && 
+				player2.quarter3 === 1 && 
+				player2.quarter2 === 1 && 
+				player2.quarter1 === 1
+				)
+			{
+				player2.lap = 1;
+				$(".winner").html("<h1>Player 2 wins!</h1>");
+				$(window).off("keydown");
+				$("button").show();
+			}	
+		});
+	}
+
+	function restartGame() {
+		player1upDown = -15;
+		player1leftRight = 300;
+		$player1.html('<img src="player1/right.png">');
+		$player1.css("left", player1leftRight + "px");
+		$player1.css("top", player1upDown + "px");
+		player1.quarter1 = 0;
+		player1.quarter2 = 0;
+		player1.quarter3 = 0;
+		player1.lap = 0;
+
+		player2upDown = -42;
+		player2leftRight = 300;
+		$player2.html('<img src="player2/right.png">');
+		$player2.css("left", player2leftRight + "px");
+		$player2.css("top", player2upDown + "px");
+		player2.quarter1 = 0;
+		player2.quarter2 = 0;
+		player2.quarter3 = 0;
+		player2.lap = 0;
+
+		$(".winner").html("<h1>Ready? Go!</h1>");
+	}
+
+	$("button").click(function handler() {
+		restartGame();
+		$("button").hide();
+		startRace();
 	});
 
+	var player1 = new Racer();
+
+	var player2 = new Racer();
+
+	var $player1 = $(".player1");
+	var player1upDown = -15;
+	var player1leftRight = 300;
+
+	var $player2 = $(".player2");
+	var player2upDown = -48;
+	var player2leftRight = 300;
+
+	$("button").hide();
+
+	startRace();
 });
